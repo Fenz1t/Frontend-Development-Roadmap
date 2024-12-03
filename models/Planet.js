5a297b7ba01e4b7aeee689f566d1cfabb62a3d14
Fedor24 <kurakin.fed@mail.ru>
2024-12-03 21:38:12 +0300

    добавленно поле text

--- models/Planet.js
+++ models/Planet.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const AsteroidSchema = new mongoose.Schema({
     name: String,
     link: String,
+    text: String,
     isCompleted: { type: Boolean, default: false },
   });
   
